207676a7303f1a1d7896d7078799ff6973f17fbb
inderdeep <inderdeepsidhu01@gmail.com>
2024-04-23 19:40:30 +0530

    learning how to write and perform tests

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test{
+    FundMe fundMe;
+
+    // this will run first
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testMinimumDollarIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public view{
+        assertEq(fundMe.i_owner(), address(this));
+        assertNotEq(fundMe.i_owner(), msg.sender);
+    }
+
+    function testPriceFeedVersion() public view {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
+    }
+}
