acbcaedab676043d826f286519ce05b6f894b908
inderdeep <inderdeepsidhu01@gmail.com>
2024-04-24 18:07:59 +0530

    making deployments by providing price feed address only as an argument to constructor

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -4,13 +4,15 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test{
     FundMe fundMe;
 
     // this will run first
     function setUp() external {
-        fundMe = new FundMe();
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testMinimumDollarIsFive() public view {
@@ -18,8 +20,8 @@ contract FundMeTest is Test{
     }
 
     function testOwnerIsMsgSender() public view{
-        assertEq(fundMe.i_owner(), address(this));
-        assertNotEq(fundMe.i_owner(), msg.sender);
+        assertEq(fundMe.i_owner(), msg.sender);
+        // assertNotEq(fundMe.i_owner(), msg.sender);
     }
 
     function testPriceFeedVersion() public view {
